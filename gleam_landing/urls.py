da6e5f0d587457260bbfb3ec3d7fc538355cfb8d
xxbadgame <yannis.nzuepro@gmail.com>
2024-06-15 01:28:24 +0200

    ImageManagement

--- gleam_landing/urls.py
+++ gleam_landing/urls.py
@@ -1,13 +1,14 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from discover.views import *
-from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
