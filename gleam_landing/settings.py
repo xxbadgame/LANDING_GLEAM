0884116913f16c22093e420648890e08d90bc8ef
xxbadgame <yannis.nzuepro@gmail.com>
2024-06-14 18:15:19 +0200

    HTMLskull

--- gleam_landing/settings.py
+++ gleam_landing/settings.py
@@ -109,3 +109,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
