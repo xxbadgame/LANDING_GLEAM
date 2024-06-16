0ba2ca81e6e4992343cc20c4e97d0e27272893dc
xxbadgame <yannis.nzuepro@gmail.com>
2024-06-17 01:38:42 +0200

    GoBackend

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=255, null=True)
-    last_name = models.CharField(max_length=255)
-    role = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):     
-        return self.username
+    pass
