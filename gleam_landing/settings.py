3d5824eeebc88f59d9b78b35d94b9734c865af82
xxbadgame <yannis.nzuepro@gmail.com>
2024-07-28 09:12:53 +0200

    Authorisation domaine Gleam

--- gleam_landing/settings.py
+++ gleam_landing/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-fbk!7t)=z)m-$8b=!+%(ke6=8tpw^+$6#hwt$#pg)ut-vdxd&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gleamwork.fr']
+ALLOWED_HOSTS = ['.gleamwork.fr','.vercel.app' ,'127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
