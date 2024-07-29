3a9ea7ee4126b64ef2997c9fd0a3ed3687abfbd7
xxbadgame <yannis.nzuepro@gmail.com>
2024-07-29 21:32:57 +0200

    postgreSQL Database change

--- gleam_landing/settings.py
+++ gleam_landing/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -61,12 +62,15 @@ WSGI_APPLICATION = 'gleam_landing.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME" : config('DB_NAME'),
+        "USER" : config('DB_USER'),
+        "PASSWORD" : config('DB_PASSWORD'),
+        "HOST" : config('DB_HOST'),
+        "PORT" : "1521"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
