0e2999a299127d5cf61724d798e0f81947cc2122
xxbadgame <yannis.nzuepro@gmail.com>
2024-08-01 07:47:47 +0200

    heroku start

--- gleam_landing/settings.py
+++ gleam_landing/settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'gleam_landing.urls'
